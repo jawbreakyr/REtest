72066fec14102d37f8e3569c6682be30dba2526d
jawbreakyr <jawbreakyr@gmail.com>
2019-05-21 21:38:39 +0800

    change to use generic class based views

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.template import loader
 from django.http import Http404
 from django.utils import timezone
+from django.views import generic
 
 from polls.models import Question, Choice
 
@@ -21,11 +22,20 @@ from polls.models import Question, Choice
 # 	return HttpResponse(template.render(context, request))
 
 # a simplified view for using a shortcut but w/o sorting
-def index(request):
-	# latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	latest_question_list = get_list_or_404(Question, )
-	context = {'latest_question_list':latest_question_list}
-	return render(request, 'polls/index.html', context)
+# def index(request):
+#	# latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	latest_question_list = get_list_or_404(Question, )
+#	context = {'latest_question_list':latest_question_list}
+#	return render(request, 'polls/index.html', context)
+
+#class based views inheriting from a generic view
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""Return the five published questions."""
+		return Question.objects.order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 # 	try:
@@ -35,13 +45,24 @@ def index(request):
 # 	return render(request, 'polls/details.html', {'question': question})
 
 # a much for simplified view for details page thats renders the data using a shortcut
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/details.html', { 'question':question })
+# def detail(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/details.html', { 'question':question })
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', { 'question': question })
+#class based views inheriting from a generic view
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/details.html'
+
+# def results(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/results.html', { 'question': question })
+
+#class based views inheriting from a generic view
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
